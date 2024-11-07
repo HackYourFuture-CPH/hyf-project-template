3774886f209c9fa48a24985adf3e769801bdd6db
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-11-08 00:42:15 +0100

    read role and other user info form cookies

--- frontend/app/dev-dashboard/page.js
+++ frontend/app/dev-dashboard/page.js
@@ -1,22 +1,40 @@
 'use client';
-import { useSession, signOut } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+import { parseCookies } from 'nookies';
+import { useRouter } from 'next/navigation';
+import { handleSignOut } from '../utils/auth';
 
-export default function MyComponent() {
-  const { data: session, status } = useSession();
+export default function DevDashboard() {
+  const [user, setUser] = useState(null);
+  const router = useRouter();
 
-  if (status === 'loading') {
+  useEffect(() => {
+    const cookies = parseCookies();
+    const userToken = cookies.userToken;
+    const userRole = cookies.userRole;
+    const userName = cookies.userName;
+
+    if (userToken && userRole === 'developer' && userName) {
+      setUser({ name: userName, role: userRole });
+    } else {
+      router.push('/login');
+    }
+  }, [router]);
+
+  if (!user) {
     return <div>Loading...</div>;
-  }
-
-  if (session) {
-    console.log('User role:', session.user);
   }
 
   return (
     <div>
-      <h1 className='m-6'>Welcome to the development dashboard {session.user.name}!</h1>
+      <h1>
+        Welcome to the development dashboard, {user.name} ({user.role})!
+      </h1>
       <button
-        onClick={() => signOut({ callbackUrl: '/' })}
+        onClick={() => {
+          handleSignOut();
+          router.push('/login');
+        }}
         className='px-4 py-2 m-6 bg-red-500 text-white rounded hover:bg-red-600'
       >
         Sign Out
