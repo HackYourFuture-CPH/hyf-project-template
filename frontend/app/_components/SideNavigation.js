204b5ba2720bc11d26af1e7df600f6bec551bb33
AliRezahassnloo2731 <reza1632731@gmail.com>
2024-11-10 20:35:45 +0100

    grid layout on sideNavigation

--- frontend/app/_components/SideNavigation.js
+++ frontend/app/_components/SideNavigation.js
@@ -2,18 +2,18 @@ import Link from "next/link";
 
 function SideNavigation() {
   return (
-    <nav className="flex flex-col gap-16 justify-start align-middle bg-primary-800 text-primary-100 w-80 h-screen text-xl rounded-2xl">
-      <ul className="">
-        <li className="py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200">
-          <Link href="/">Dashbord</Link>
+    <nav className="col-span-3 row-span-8 row-start-4 bg-primary-800 text-primary-100 text-xl">
+      <ul className="grid grid-cols-3 grid-rows-7 mt-8 h-2/4">
+        <li className="col-span-3 flex items-center justify-center hover:bg-primary-900 w-full hover:text-primary-100 transition-colors  gap-4 font-semibold text-primary-200 ">
+          <Link href="/">Dashboard</Link>
         </li>
-        <li className="py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200">
+        <li className="col-span-3 row-start-2 flex items-center justify-center hover:bg-primary-900 hover:text-primary-100 transition-colors gap-4 font-semibold text-primary-200 ">
           <Link href="/projects">Projects</Link>
         </li>
-        <li className="py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200">
+        <li className="col-span-3 row-start-3 flex items-center justify-center hover:bg-primary-900 hover:text-primary-100 transition-colors  gap-4 font-semibold text-primary-200 ">
           <Link href="/clients">Clients</Link>
         </li>
-        <li className="py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200">
+        <li className="col-span-3 row-start-6 flex items-center justify-center hover:bg-primary-900 hover:text-primary-100 transition-colors  gap-4 font-semibold text-primary-200 ">
           <Link href="/setting">Setting</Link>
         </li>
       </ul>
