913ef3f05094513caadf378d4757d46fe9a68838
AliRezahassnloo2731 <reza1632731@gmail.com>
2024-11-10 14:59:46 +0100

    modify metadata add styles folder

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,10 +1,10 @@
-'use client';
-import React, { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import RoleSelection from './components/RoleSelection';
-import SignIn from './components/SignIn';
-import SignUp from './components/SignUp';
-import { handleSignUp, handleSignIn } from './utils/auth';
+"use client";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import RoleSelection from "./_components/RoleSelection";
+import SignIn from "./_components/SignIn";
+import SignUp from "./_components/SignUp";
+import { handleSignUp, handleSignIn } from "./utils/auth";
 
 const Home = () => {
   const [role, setRole] = useState(null);
@@ -12,10 +12,25 @@ const Home = () => {
   const [error, setError] = useState(null);
   const router = useRouter();
 
-  const handleSignUpClick = async (name, email, password, phone) => {
-    const result = await handleSignUp(name, email, password, phone, role);
+  const handleSignUpClick = async (
+    name,
+    email,
+    password,
+    phone
+  ) => {
+    const result = await handleSignUp(
+      name,
+      email,
+      password,
+      phone,
+      role
+    );
     if (result.success) {
-      router.push(role === 'developer' ? '/dev-dashboard' : '/client-dashboard');
+      router.push(
+        role === "developer"
+          ? "/dev-dashboard"
+          : "/client-dashboard"
+      );
     } else {
       setError(result.message);
     }
@@ -24,36 +39,56 @@ const Home = () => {
   const handleSignInClick = async (email, password) => {
     const result = await handleSignIn(email, password);
     if (result.success) {
-      router.push(result.role === 'developer' ? '/dev-dashboard' : '/client-dashboard');
+      router.push(
+        result.role === "developer"
+          ? "/dev-dashboard"
+          : "/client-dashboard"
+      );
     } else {
       setError(result.message);
     }
   };
 
   return (
-    <div className='min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-blue-500 via-purple-600 to-blue-500 p-4'>
-      <div className='bg-white shadow-lg rounded-lg p-8 max-w-lg w-full'>
-        <h1 className='text-3xl font-bold text-gray-800 text-center mb-6'>
-          {isSignUp ? 'Create Your Account' : 'Welcome to WFlance!'}
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-blue-500 via-purple-600 to-blue-500 p-4">
+      <div className="bg-white shadow-lg rounded-lg p-8 max-w-lg w-full">
+        <h1 className="text-3xl font-bold text-gray-800 text-center mb-6">
+          {isSignUp
+            ? "Create Your Account"
+            : "Welcome to WFlance!"}
         </h1>
 
         {isSignUp ? (
           !role ? (
             <RoleSelection setRole={setRole} />
           ) : (
-            <SignUp handleSignUp={handleSignUpClick} setRole={setRole} error={error} role={role} />
+            <SignUp
+              handleSignUp={handleSignUpClick}
+              setRole={setRole}
+              error={error}
+              role={role}
+            />
           )
         ) : (
-          <SignIn handleSignIn={handleSignInClick} error={error} />
+          <SignIn
+            handleSignIn={handleSignInClick}
+            error={error}
+          />
         )}
 
-        {error && <p className='text-red-500 text-center mt-4'>{error}</p>}
+        {error && (
+          <p className="text-red-500 text-center mt-4">
+            {error}
+          </p>
+        )}
 
         <button
-          onClick={() => setIsSignUp(prev => !prev)}
-          className='mt-6 w-full underline text-grey-00 font-semibold py-2 rounded-lg transition duration-300 ease-in-out'
+          onClick={() => setIsSignUp((prev) => !prev)}
+          className="mt-6 w-full underline text-grey-00 font-semibold py-2 rounded-lg transition duration-300 ease-in-out"
         >
-          {isSignUp ? 'Have an account? Sign In' : 'Need an account? Sign Up'}
+          {isSignUp
+            ? "Have an account? Sign In"
+            : "Need an account? Sign Up"}
         </button>
       </div>
     </div>
--- frontend/middleware.js
+++ frontend/middleware.js
@@ -1,28 +1,41 @@
-import { NextResponse } from 'next/server';
-import { parseCookies } from 'nookies';
+import { NextResponse } from "next/server";
+import { parseCookies } from "nookies";
 
 export function middleware(req) {
   const { pathname } = req.nextUrl;
 
-  const protectedRoutes = ['/dev-dashboard', '/client-dashboard'];
+  const protectedRoutes = [
+    "/dev-dashboard",
+    "/client-dashboard",
+  ];
 
-  if (protectedRoutes.some(route => pathname.startsWith(route))) {
+  if (
+    protectedRoutes.some((route) =>
+      pathname.startsWith(route)
+    )
+  ) {
     // Get the cookies
     const cookies = parseCookies(req);
     const userToken = cookies.userToken;
     const userRole = cookies.userRole;
 
     if (!userToken) {
-      return NextResponse.redirect('/login');
+      return NextResponse.redirect("/login");
     }
 
     // redirect based on role to the correct dashboard if the role doesn't match
-    if (pathname.startsWith('/dev-dashboard') && userRole !== 'developer') {
-      return NextResponse.redirect('/client-dashboard');
+    if (
+      pathname.startsWith("/dev-dashboard") &&
+      userRole !== "developer"
+    ) {
+      return NextResponse.redirect("/client-dashboard");
     }
 
-    if (pathname.startsWith('/client-dashboard') && userRole !== 'client') {
-      return NextResponse.redirect('/dev-dashboard');
+    if (
+      pathname.startsWith("/client-dashboard") &&
+      userRole !== "client"
+    ) {
+      return NextResponse.redirect("/dev-dashboard");
     }
   }
 
@@ -31,5 +44,8 @@ export function middleware(req) {
 
 // paths to be protected
 export const config = {
-  matcher: ['/dev-dashboard/:path*', '/client-dashboard/:path*'],
+  matcher: [
+    "/dev-dashboard/:path*",
+    "/client-dashboard/:path*",
+  ],
 };
