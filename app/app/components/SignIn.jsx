4ac3ee91f085601538cc647b9c3c1b7bd7ca2e72
Moeen Heydari <moeentk@gmail.com>
2024-11-11 00:42:17 +0100

    111124: search page added

--- app/app/components/SignIn.jsx
+++ app/app/components/SignIn.jsx
@@ -4,27 +4,29 @@ import Button from "./Button";
 
 const SignIn = () => {
     return (
-        <section className={styles.signIn}>
-            <h2 className={styles.signInTitle}>Join Leaf Notes</h2>
-            <p className={styles.signInDescription}>
-                Already a member? Sign in to continue your journey, or create a free account to
-                start your own book log today!
-            </p>
-            <div className={styles.buttonContainer}>
-                <Button variant="primary" className={styles.signInButton}>
-                    SIGN IN
-                </Button>
-                <Button variant="secondary" className={styles.signUpButton}>
-                    SIGN UP
-                </Button>
-            </div>
-            <h3 className={styles.benefitsTitle}>Benefits for Signing Up:</h3>
-            <ul className={styles.benefitsList}>
-                <li>Access your personal book log anytime, anywhere.</li>
-                <li>Set and track reading goals.</li>
-                <li>Save favorite quotes and personal reflections.</li>
-            </ul>
-        </section>
+        <div className={styles.container}>
+            <section className={styles.signIn}>
+                <h2 className={styles.signInTitle}>Join Leaf Notes</h2>
+                <p className={styles.signInDescription}>
+                    Already a member? Sign in to continue your journey, or create a free account to
+                    start your own book log today!
+                </p>
+                <div className={styles.buttonContainer}>
+                    <Button variant="primary" className={styles.signInButton}>
+                        SIGN IN
+                    </Button>
+                    <Button variant="secondary" className={styles.signUpButton}>
+                        SIGN UP
+                    </Button>
+                </div>
+                <h3 className={styles.benefitsTitle}>Benefits for Signing Up:</h3>
+                <ul className={styles.benefitsList}>
+                    <li>Access your personal book log anytime, anywhere.</li>
+                    <li>Set and track reading goals.</li>
+                    <li>Save favorite quotes and personal reflections.</li>
+                </ul>
+            </section>
+        </div>
     );
 };
 
--- app/app/page.jsx
+++ app/app/page.jsx
@@ -1,23 +1,18 @@
 import AppLayoutContainer from "./components/AppLayoutContainer";
 import MainPart from "./components/MainPart";
-import Search from "./components/Search";
 import About from "./components/About";
 import SignIn from "./components/SignIn";
 import OurTeam from "./components/OurTeam";
 import MemorableLines from "./components/MemorableLines";
-import styles from "./page.module.css";
 
 export default function Home() {
     return (
         <AppLayoutContainer>
-            <div className={styles.container}>
-                <MainPart />
-                <Search />
-                <About />
-                <SignIn />
-                <OurTeam />
-                <MemorableLines />
-            </div>
+            <MainPart />
+            <About />
+            <SignIn />
+            <OurTeam />
+            <MemorableLines />
         </AppLayoutContainer>
     );
 }
