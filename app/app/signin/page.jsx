710a1f63a528fdf4f93541218a0725b1e2c7d623
Saikiruthiga <saikiruthiga06@gmail.com>
2024-11-11 14:22:28 +0100

    Added background color

--- app/app/signin/page.jsx
+++ app/app/signin/page.jsx
@@ -115,199 +115,210 @@ const SignUp = () => {
   return (
     <Container
       sx={{
+        backgroundColor: "#B0E0E6",
+        minHeight: "100vh",
+        minWidth: "100vw",
         display: "flex",
-        background: "#F0D2D6",
-        borderRadius: "10px",
-        top: "50%",
-        position: "absolute",
-        transform: "translate(0,-50%)",
-        right: "5%",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
       <Container
         sx={{
           display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          marginTop: 5,
-        }}
-      >
-        <Typography
-          variant="h5"
-          sx={{
-            fontFamily: "jacques Francois",
-            marginBottom: 2,
-            color: "#3C3C3C",
-            fontWeight: "500",
-            whiteSpace: "pre-line",
-            letterSpacing: "0.5px",
-            lineHeight: 1.5,
-            padding: "10px 20px",
-            backgroundColor: "#F5ECE8",
-            borderRadius: "8px",
-            boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
-          }}
-        >
-          <Box sx={{ fontWeight: "bold", fontStyle: "italic" }}>
-            Welcome to LeafNotes {"\n"}
-          </Box>
-          Track what you've read and {"\n"}what's next.
-        </Typography>
-
-        <Box sx={{ width: "500px", height: "auto" }}>
-          <img src="./books.jpg" style={{ width: "100%", height: "auto" }} />
-        </Box>
-      </Container>
-      <Container
-        maxWidth="md"
-        sx={{
-          padding: 3,
-          background: "#EAD3C4",
+          background: "#F0D2D6",
           borderRadius: "10px",
-          marginRight: "-25px",
+          top: "50%",
+          position: "absolute",
+          transform: "translate(0,-50%)",
+          right: "5%",
         }}
       >
-        <Typography
-          variant="h4"
+        <Container
           sx={{
-            textAlign: "center",
-            margin: "15px",
-            fontFamily: "jacques Francois",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            marginTop: 5,
           }}
         >
-          Create Account{" "}
-        </Typography>{" "}
-        <Box sx={{ display: "flex", width: "100%", mb: 1 }}>
-          {" "}
-          <Grid2 container spacing={2} sx={{ width: "100%" }}>
+          <Typography
+            variant="h5"
+            sx={{
+              fontFamily: "jacques Francois",
+              marginBottom: 2,
+              color: "#3C3C3C",
+              fontWeight: "500",
+              whiteSpace: "pre-line",
+              letterSpacing: "0.5px",
+              lineHeight: 1.5,
+              padding: "10px 20px",
+              backgroundColor: "#F5ECE8",
+              borderRadius: "8px",
+              boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+            }}
+          >
+            <Box sx={{ fontWeight: "bold", fontStyle: "italic" }}>
+              Welcome to LeafNotes {"\n"}
+            </Box>
+            Track what you've read and {"\n"}what's next.
+          </Typography>
+
+          <Box sx={{ width: "500px", height: "auto" }}>
+            <img src="./books.jpg" style={{ width: "100%", height: "auto" }} />
+          </Box>
+        </Container>
+        <Container
+          maxWidth="md"
+          sx={{
+            padding: 3,
+            background: "#EAD3C4",
+            borderRadius: "10px",
+            marginRight: "-25px",
+          }}
+        >
+          <Typography
+            variant="h4"
+            sx={{
+              textAlign: "center",
+              margin: "15px",
+              fontFamily: "jacques Francois",
+            }}
+          >
+            Create Account{" "}
+          </Typography>{" "}
+          <Box sx={{ display: "flex", width: "100%", mb: 1 }}>
             {" "}
-            <Grid2 xs={6} sx={{ flexGrow: 1, maxWidth: "50%" }}>
+            <Grid2 container spacing={2} sx={{ width: "100%" }}>
               {" "}
-              <TextField
-                variant="outlined"
-                required
-                id="firstName"
-                label="First Name"
-                name="firstName"
-                autoComplete="fname"
-                fullWidth
-                value={data.firstName}
-                onChange={handleChange}
-                error={errors.firstName}
-                helperText={errors.firstName}
-                sx={{
-                  "& .MuiOutlinedInput-root": {
-                    borderRadius: 2,
-                  },
-                }}
-              />{" "}
+              <Grid2 xs={6} sx={{ flexGrow: 1, maxWidth: "50%" }}>
+                {" "}
+                <TextField
+                  variant="outlined"
+                  required
+                  id="firstName"
+                  label="First Name"
+                  name="firstName"
+                  autoComplete="fname"
+                  fullWidth
+                  value={data.firstName}
+                  onChange={handleChange}
+                  error={errors.firstName}
+                  helperText={errors.firstName}
+                  sx={{
+                    "& .MuiOutlinedInput-root": {
+                      borderRadius: 2,
+                    },
+                  }}
+                />{" "}
+              </Grid2>{" "}
+              <Grid2 xs={6} sx={{ flexGrow: 1, maxWidth: "50%" }}>
+                {" "}
+                <TextField
+                  variant="outlined"
+                  required
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="lname"
+                  fullWidth
+                  value={data.lastName}
+                  onChange={handleChange}
+                  error={!!errors.lastName}
+                  helperText={errors.lastName}
+                  sx={{
+                    "& .MuiOutlinedInput-root": {
+                      borderRadius: 2,
+                    },
+                  }}
+                />{" "}
+              </Grid2>{" "}
             </Grid2>{" "}
-            <Grid2 xs={6} sx={{ flexGrow: 1, maxWidth: "50%" }}>
-              {" "}
-              <TextField
-                variant="outlined"
-                required
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
-                fullWidth
-                value={data.lastName}
-                onChange={handleChange}
-                error={!!errors.lastName}
-                helperText={errors.lastName}
-                sx={{
-                  "& .MuiOutlinedInput-root": {
-                    borderRadius: 2,
-                  },
-                }}
-              />{" "}
-            </Grid2>{" "}
-          </Grid2>{" "}
-        </Box>{" "}
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-          autoComplete="email"
-          value={data.email}
-          onChange={handleChange}
-          error={!!errors.email}
-          helperText={errors.email}
-          sx={{
-            "& .MuiOutlinedInput-root": {
+          </Box>{" "}
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            value={data.email}
+            onChange={handleChange}
+            error={!!errors.email}
+            helperText={errors.email}
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                borderRadius: 2,
+              },
+            }}
+          />{" "}
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            value={data.username}
+            onChange={handleChange}
+            error={!!errors.username}
+            helperText={errors.username}
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                borderRadius: 2,
+              },
+            }}
+          />{" "}
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="new-password"
+            value={data.password}
+            onChange={handleChange}
+            error={!!errors.password}
+            helperText={errors.password}
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                borderRadius: 2,
+              },
+            }}
+          />{" "}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            sx={{
+              background: "grey",
               borderRadius: 2,
-            },
-          }}
-        />{" "}
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          id="username"
-          label="Username"
-          name="username"
-          autoComplete="username"
-          value={data.username}
-          onChange={handleChange}
-          error={!!errors.username}
-          helperText={errors.username}
-          sx={{
-            "& .MuiOutlinedInput-root": {
-              borderRadius: 2,
-            },
-          }}
-        />{" "}
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          autoComplete="new-password"
-          value={data.password}
-          onChange={handleChange}
-          error={!!errors.password}
-          helperText={errors.password}
-          sx={{
-            "& .MuiOutlinedInput-root": {
-              borderRadius: 2,
-            },
-          }}
-        />{" "}
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="primary"
-          sx={{
-            background: "grey",
-            borderRadius: 2,
-            mt: 3,
-            mb: 2,
-            fontFamily: "jacques Francois",
-            textTransform: "none",
-            fontSize: "20px",
-          }}
-          onClick={handleSubmit}
-        >
-          Submit{" "}
-        </Button>{" "}
-        <Typography variant="h8">
-          Already have an account?{" "}
-          <a href="/login" target="_blank" style={{ color: "blueviolet" }}>
-            Login{" "}
-          </a>{" "}
-        </Typography>{" "}
-      </Container>{" "}
+              mt: 3,
+              mb: 2,
+              fontFamily: "jacques Francois",
+              textTransform: "none",
+              fontSize: "20px",
+            }}
+            onClick={handleSubmit}
+          >
+            Submit{" "}
+          </Button>{" "}
+          <Typography variant="h8">
+            Already have an account?{" "}
+            <a href="/login" target="_blank" style={{ color: "blueviolet" }}>
+              Login{" "}
+            </a>{" "}
+          </Typography>{" "}
+        </Container>{" "}
+      </Container>
     </Container>
   );
 };
