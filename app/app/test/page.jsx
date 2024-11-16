773ae7564ca91c15cce8ccbca9c8a8934875a952
Tanya Dewland <tanya.dewland@gmail.com>
2024-11-16 16:55:42 +0100

    Example of fetching data using useAuth in /test/page.jsx

--- app/app/test/page.jsx
+++ app/app/test/page.jsx
@@ -1,26 +1,31 @@
 "use client";
 import { useAuth } from "../contexts/AuthContext";
 
-export default function Test() {
-  const { currentUser, logout, loading } = useAuth();
+function ProfileComponent() {
+  const { currentUser, loading } = useAuth();
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
+  if (!currentUser) {
+    return <div>Please log in to view your profile</div>;
+  }
+
   return (
     <div>
-      <h1>Welcome to the App</h1>
-      {currentUser ? (
-        <div>
-          <p>Hello, {currentUser.user.username}!</p>
-          <button onClick={logout}>Logout</button>
-        </div>
-      ) : (
-        <div>
-          <p>Please log in to continue</p>
-        </div>
+      <h1>My Profile</h1>
+      <div>Username: {currentUser.user.username}</div>
+      <div>Email: {currentUser.user.email}</div>
+      {currentUser.user.first_name && (
+        <div>First Name: {currentUser.user.first_name}</div>
       )}
+      {currentUser.last_name && (
+        <div>Last Name: {currentUser.user.last_name}</div>
+      )}
+      {/* Add any other currentUser fields you want to display */}
     </div>
   );
 }
+
+export default ProfileComponent;
