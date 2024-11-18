b9aad6bced69e9f255634d29dcf404d7e1665678
Tanya Dewland <tanya.dewland@gmail.com>
2024-11-18 20:59:12 +0100

    Trying to set up cookies

--- api/src/controllers/userBooksController.js
+++ api/src/controllers/userBooksController.js
@@ -3,6 +3,28 @@ import fetch from "node-fetch";
 
 const googleBooksApiKey = process.env.GOOGLE_BOOKS_API_KEY;
 
+const fetchGoogleBooksDetails = async (googleBooksId) => {
+  try {
+    const googleBooksResponse = await fetch(
+      `https://www.googleapis.com/books/v1/volumes/${googleBooksId}?key=${googleBooksApiKey}`
+    );
+    const data = await googleBooksResponse.json();
+    if (!data.volumeInfo) {
+      throw new Error("Book details not found");
+    }
+    return {
+      title: data.volumeInfo.title,
+      author: data.volumeInfo.authors ? data.volumeInfo.authors[0] : "Unknown",
+      genre: data.volumeInfo.categories ? data.volumeInfo.categories[0] : null,
+      cover_image: data.volumeInfo.imageLinks?.thumbnail || null,
+      description: data.volumeInfo.description || null,
+    };
+  } catch (error) {
+    console.error("Error fetching book details from Google Books:", error);
+    throw new Error("Error fetching book details from Google Books");
+  }
+};
+
 export const addBookToUser = async (req, res) => {
   const userId = req.user.userId;
   const userRole = req.user.role;
@@ -17,6 +39,7 @@ export const addBookToUser = async (req, res) => {
     start_date = null,
     end_date = null,
   } = req.body;
+
   if (userRole !== "admin" && userRole !== "user") {
     return res
       .status(403)
@@ -25,39 +48,21 @@ export const addBookToUser = async (req, res) => {
   if (!google_books_id) {
     return res.status(400).json({ error: "google_book_id is required" });
   }
-  let bookData = {
-    google_books_id: google_books_id,
-    title,
-    author,
-    genre,
-    cover_image,
-    description,
-  };
 
-  if (!title || !author || !genre || !cover_image || !description) {
-    try {
-      const googleBooksResponse = await fetch(
-        `https://www.googleapis.com/books/v1/volumes/${google_books_id}?key=${googleBooksApiKey}`
-      );
-      const googleBook = await googleBooksResponse.json();
-      if (!googleBook) {
-        return res.status(404).json({ error: "Book not found" });
-      }
-      const volumeInfo = googleBook.volumeInfo;
-      bookData.title = title || volumeInfo.title;
-      bookData.author =
-        author || volumeInfo.authors ? volumeInfo.authors[0] : "Unknown";
-      bookData.genre =
-        genre || volumeInfo.categories ? volumeInfo.categories[0] : null;
-      bookData.cover_image =
-        cover_image || volumeInfo.imageLinks?.thumbnail || null;
-      bookData.description = description || volumeInfo.description || null;
-    } catch (error) {
-      console.error("Error fetching book details from Google Books:", error);
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-  }
   try {
+    let bookData = {
+      google_books_id,
+      title,
+      author,
+      genre,
+      cover_image,
+      description,
+    };
+
+    if (!title || !author || !genre || !cover_image || !description) {
+      const googleBooksDetails = await fetchGoogleBooksDetails(google_books_id);
+      bookData = { ...bookData, ...googleBooksDetails };
+    }
     const result = await knex.transaction(async (trx) => {
       let book = await trx("Books")
         .where({ google_books_id: bookData.google_books_id })
@@ -121,11 +126,9 @@ export const getUserBooks = async (req, res) => {
 
   try {
     if (userRole !== "admin" && userRole !== "user") {
-      return res
-        .status(403)
-        .json({
-          error: "Forbidden: You do not have permission to view books.",
-        });
+      return res.status(403).json({
+        error: "Forbidden: You do not have permission to view books.",
+      });
     }
     if (userRole === "admin") {
       const allUserBooks = await knex("Books")
