0f3f0a9f120d8f3e0fd8b13bb70eeff4a21e104e
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-11-19 13:36:28 +0100

    prevent logged-in users from accessing login/signup pages

--- frontend/app/dev-dashboard/page.js
+++ frontend/app/dev-dashboard/page.js
@@ -1,11 +1,14 @@
-'use client';
-import LogOutButton from '../_components/LogOutButton';
+"use client";
+import { useRouter } from "next/navigation";
+import LogOutButton from "../_components/LogOutButton";
 
 export default function DevDashboard() {
+  const router = useRouter();
   return (
     <div>
       <h1>Welcome to the development dashboard!</h1>
       <LogOutButton />
+      <button onClick={() => router.push("/")}>Go to home dashboard</button>
     </div>
   );
 }
