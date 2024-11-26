13c1eba95abf3571a7569ca01d6e61d74d3b9cdf
Moeen Heydari <moeentk@gmail.com>
2024-11-27 00:42:48 +0100

    271124: quotesController.js (getUserQuotes and addQuote) are updated

--- app/app/components/FavoriteQuote.jsx
+++ app/app/components/FavoriteQuote.jsx
@@ -5,60 +5,57 @@ import { useAuth } from "../contexts/AuthContext";
 import styles from "./FavoriteQuote.module.css";
 
 const FavoriteQuote = ({ bookId, closeModal }) => {
-  const { currentUser } = useAuth();
-  const [quoteText, setQuoteText] = useState("");
+    const { currentUser } = useAuth();
+    const [quoteText, setQuoteText] = useState("");
 
-  const [error, setError] = useState(null);
-  const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(null);
+    const [success, setSuccess] = useState(false);
 
-  const handleSubmit = async () => {
-    if (!quoteText) {
-      setError("Quote text is required.");
-      return;
-    }
-
-    try {
-      await axios.post(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}/quotes/add-quote`,
-        {
-          bookId,
-          userId: currentUser.user.id,
-          quoteText,
+    const handleSubmit = async () => {
+        if (!quoteText) {
+            setError("Quote text is required.");
+            return;
         }
-      );
 
-      setSuccess(true);
-      setTimeout(() => {
-        window.location.reload();
-      }, 1500); // Optionally delay to show success message
-    } catch (err) {
-      console.error("Error adding quote:", err);
-      setError("Failed to add quote.");
-    }
-  };
+        try {
+            await axios.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/quotes/add-quote`, {
+                bookId,
+                userId: currentUser.user.id,
+                content: quoteText,
+            });
 
-  return (
-    <div className={styles.modalOverlay}>
-      <div className={styles.modalContent}>
-        <button onClick={closeModal} className={styles.closeButton}>
-          &times;
-        </button>
-        <h2>Add Favorite Quote</h2>
-        {error && <p className={styles.error}>{error}</p>}
-        {success && <p className={styles.success}>Quote added successfully!</p>}
-        <textarea
-          placeholder="Enter quote text"
-          value={quoteText}
-          onChange={(e) => setQuoteText(e.target.value)}
-          className={styles.textarea}
-        />
+            setSuccess(true);
+            setTimeout(() => {
+                window.location.reload();
+            }, 1500);
+        } catch (err) {
+            console.error("Error adding quote:", err);
+            setError("Failed to add quote.");
+        }
+    };
 
-        <Button onClick={handleSubmit} className={styles.submitButton}>
-          Add Quote
-        </Button>
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.modalOverlay}>
+            <div className={styles.modalContent}>
+                <button onClick={closeModal} className={styles.closeButton}>
+                    &times;
+                </button>
+                <h2>Add Favorite Quote</h2>
+                {error && <p className={styles.error}>{error}</p>}
+                {success && <p className={styles.success}>Quote added successfully!</p>}
+                <textarea
+                    placeholder="Enter quote text"
+                    value={quoteText}
+                    onChange={(e) => setQuoteText(e.target.value)}
+                    className={styles.textarea}
+                />
+
+                <Button onClick={handleSubmit} className={styles.submitButton}>
+                    Add Quote
+                </Button>
+            </div>
+        </div>
+    );
 };
 
 export default FavoriteQuote;
--- app/app/components/Profile.jsx
+++ app/app/components/Profile.jsx
@@ -10,107 +10,95 @@ import { makeRequest } from "../utils/makeRequest";
 import styles from "./Profile.module.css";
 
 const Profile = () => {
-  const [userData, setUserData] = useState(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [favoriteGenre, setFavoriteGenre] = useState(null);
-  const { booksCount, loading, error } = useBookshelf();
+    const [userData, setUserData] = useState(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [favoriteGenre, setFavoriteGenre] = useState(null);
+    const { booksCount, loading, error } = useBookshelf();
 
-  useEffect(() => {
-    const fetchUserProfile = async () => {
-      try {
-        const response = await makeRequest(`/users/profile`, {}, "GET");
-        setUserData(response.user);
-      } catch (err) {
-        console.error("Error fetching user profile:", err);
-        setUserData("Error fetching user profile.");
-      }
-    };
+    useEffect(() => {
+        const fetchUserProfile = async () => {
+            try {
+                const response = await makeRequest(`/users/profile`, {}, "GET");
+                setUserData(response.user);
+            } catch (err) {
+                console.error("Error fetching user profile:", err);
+                setUserData("Error fetching user profile.");
+            }
+        };
 
-    const fetchFavoriteGenre = async () => {
-      try {
-        const response = await makeRequest(
-          "/api/user-books/favorite-genre",
-          {},
-          "GET"
-        );
-        setFavoriteGenre(response.favoriteGenre);
-      } catch (err) {
-        console.error("Error fetching favorite genre:", err);
-        setFavoriteGenre("Not available.");
-      }
-    };
+        const fetchFavoriteGenre = async () => {
+            try {
+                const response = await makeRequest("/api/user-books/favorite-genre", {}, "GET");
+                setFavoriteGenre(response.favoriteGenre);
+            } catch (err) {
+                console.error("Error fetching favorite genre:", err);
+                setFavoriteGenre("Not available.");
+            }
+        };
 
-    fetchUserProfile();
-    fetchFavoriteGenre();
-  }, []);
+        fetchUserProfile();
+        fetchFavoriteGenre();
+    }, []);
 
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>{error}</div>;
-  if (!userData) return <div>No user data available.</div>;
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>{error}</div>;
+    if (!userData) return <div>No user data available.</div>;
 
-  return (
-    <Box sx={{ display: "flex", flexDirection: "column", gap: 3 }}>
-      <div className={styles.Profile}>
-        <Button onClick={openModal}>EDIT PROFILE</Button>
-        <div className={styles.imageContainer}>
-          <img
-            alt="Profile"
-            src={userData.profileImageUrl || "/profile-default.svg"}
-            className={styles.profileImage}
-          />
-        </div>
-        <div className={styles.userDetailsContainer}>
-          <div>
-            <strong>Name:</strong>{" "}
-            {`${userData.firstName} ${userData.lastName}`}
-          </div>
-          <div>
-            <strong>Username:</strong> {userData.username}
-          </div>
-          <div>
-            <strong>Email:</strong> {userData.email}
-          </div>
-          <div>
-            <strong>About:</strong>{" "}
-            {userData.about || "No information available."}
-          </div>
-          <div>
-            <strong>Favorite Genre:</strong> {favoriteGenre || "Not available"}
-          </div>
-          <ul>
-            {userData.favoriteBooks &&
-              userData.favoriteBooks.length > 0 &&
-              userData.favoriteBooks.map((book, idx) => (
-                <li key={idx}>{book}</li>
-              ))}
-          </ul>
-          <div>
-            <strong>Books Read:</strong> {booksCount}
-          </div>
-        </div>
+    return (
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 3 }}>
+            <div className={styles.Profile}>
+                <div className={styles.Welcome}>
+                    <strong>Welcome {userData.username}</strong>
+                    <button className={styles.editButton} onClick={openModal}>
+                        ✏️
+                    </button>
+                </div>
+                <div className={styles.imageContainer}>
+                    <img
+                        alt="Profile"
+                        src={userData.profileImageUrl || "/profile-default.svg"}
+                        className={styles.profileImage}
+                    />
+                </div>
+                <div className={styles.userDetailsContainer}>
+                    <div>
+                        <strong>About:</strong> {userData.about || "No information available."}
+                    </div>
+                    <div>
+                        <strong>Favorite Genre:</strong> {favoriteGenre || "Not available"}
+                    </div>
+                    <ul>
+                        {userData.favoriteBooks &&
+                            userData.favoriteBooks.length > 0 &&
+                            userData.favoriteBooks.map((book, idx) => <li key={idx}>{book}</li>)}
+                    </ul>
+                    <div>
+                        <strong>Books Read:</strong> {booksCount}
+                    </div>
+                </div>
 
-        {isModalOpen && (
-          <EditProfile
-            isOpen={isModalOpen}
-            onClose={closeModal}
-            userData={userData} // Pass current user data to modal
-            onSave={(updatedData) => {
-              setUserData(updatedData);
-              closeModal();
-            }}
-          />
-        )}
-      </div>
-      <GoalProvider>
-        <Box sx={{ mt: 3 }}>
-          <GoalsWidget />
+                {isModalOpen && (
+                    <EditProfile
+                        isOpen={isModalOpen}
+                        onClose={closeModal}
+                        userData={userData} // Pass current user data to modal
+                        onSave={(updatedData) => {
+                            setUserData(updatedData);
+                            closeModal();
+                        }}
+                    />
+                )}
+            </div>
+            <GoalProvider>
+                <Box sx={{ mt: 3 }}>
+                    <GoalsWidget />
+                </Box>
+            </GoalProvider>
         </Box>
-      </GoalProvider>
-    </Box>
-  );
+    );
 };
 
 export default Profile;
