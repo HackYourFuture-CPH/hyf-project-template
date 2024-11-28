8c018c844ad75f6d54a7c31752f493d204646dfd
Tanya Dewland <tanya.dewland@gmail.com>
2024-11-28 22:47:27 +0100

    Expand filterbar

--- app/app/components/AddBookToBookshelf/Filterbar.jsx
+++ app/app/components/AddBookToBookshelf/Filterbar.jsx
@@ -1,21 +1,62 @@
 import { filterOptions } from "./constants";
 
-const Filter = ({ filter, setFilter }) => {
+const FilterBar = ({ filters, onFilterChange }) => {
   return (
     <div className="filter">
-      <label htmlFor="filter">Filter:</label>
+      <label htmlFor="language">Language:</label>
       <select
-        name="filter"
-        id="filter"
-        value={filter}
-        onChange={(e) => setFilter(e.target.value)}
+        name="language"
+        id="language"
+        value={filters.language}
+        onChange={(e) => onFilterChange("language", e.target.value)}
       >
-        {filterOptions.map((option) => (
+        {filterOptions.languages.map((option) => (
           <option key={option.value} value={option.value}>
             {option.label}
+          </option>
+        ))}
+      </select>
+      <label htmlFor="genre">Genre:</label>
+      <select
+        name="genre"
+        id="genre"
+        value={filters.genre}
+        onChange={(e) => onFilterChange("genre", e.target.value)}
+      >
+        {filterOptions.genres.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+      <label htmlFor="filterType">Book Type:</label>
+      <select
+        name="filterType"
+        id="filterType"
+        value={filters.filterType}
+        onChange={(e) => onFilterChange("filterType", e.target.value)}
+      >
+        {filterOptions.filterTypes.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+      <label htmlFor="maxResults">Show:</label>
+      <select
+        name="maxResults"
+        id="maxResults"
+        value={filters.maxResults}
+        onChange={(e) => onFilterChange("maxResults", Number(e.target.value))}
+      >
+        {filterOptions.maxResults.map((option) => (
+          <option key={value} value={value}>
+            {value} results
           </option>
         ))}
       </select>
     </div>
   );
 };
+
+export default FilterBar;
