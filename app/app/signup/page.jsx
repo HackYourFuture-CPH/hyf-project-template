6b9bdf032febfc41338c809a64fcbb681063ab02
NadaJrad <nadajradbenaicha@gmail.com>
2024-11-28 16:06:57 +0100

    styling the login component to be same with login

--- app/app/signup/page.jsx
+++ app/app/signup/page.jsx
@@ -93,13 +93,13 @@ const SignUp = () => {
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-          bgcolor: theme === "dark" ? "var(--background-dark)" : "#F0D2D6", // Adjust background color based on theme
+          bgcolor: theme === "dark" ? "#000000" : "#F0D2D6", // Adjust background color based on theme
         }}
       >
         <Container
           sx={{
             display: "flex",
-            background: theme === "dark" ? "#333333" : "#F5ECE8", // Dark mode background
+            background: theme === "dark" ? "#111112" : "#F5ECE8", // Dark mode background
             borderRadius: 2,
             boxShadow:
               theme === "dark"
@@ -150,7 +150,7 @@ const SignUp = () => {
             maxWidth="md"
             sx={{
               padding: 3,
-              background: theme === "dark" ? "#444444" : "#EAD3C4", // Adjust background color for form container
+              background: theme === "dark" ? "#000000" : "#EAD3C4", // Adjust background color for form container
               marginRight: "-25px",
               borderRadius: 2,
               boxShadow:
