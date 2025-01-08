4d96965f7b5389e6ce0b26fec494c2b6f64d8d02
Halyna Bondar <galya.marusyak@gmail.com>
2025-01-08 16:14:53 +0100

    Delete redundant code and files

--- app/components/TestPage/TestPage.jsx
+++ app/components/TestPage/TestPage.jsx
@@ -1,22 +1,7 @@
 "use client"
-import { useEffect, useState } from "react";
-import api from "@/utils/api";
 
-// This page can be deleted once you add your own page
 function TestPage() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    async function fetchMessage() {
-      const response = await fetch(api("/nested"));
-      const { message } = await response.json();
-      setMessage(message);
-    }
-
-    fetchMessage();
-  }, []);
-
-  return <span className="message">{message}</span>;
+  return <></>;
 }
 
 export default TestPage;
