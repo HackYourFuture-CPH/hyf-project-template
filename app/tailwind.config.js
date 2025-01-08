dc34624de9f699e091b58df2d5e186a67b75eb32
Halyna Bondar <galya.marusyak@gmail.com>
2025-01-08 16:14:08 +0100

    Install Tailwind

---
+++ app/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,css,html,jsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
