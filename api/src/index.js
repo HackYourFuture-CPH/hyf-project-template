c52a2e022720c38ab2738b045a21979871bd4685
Bhumika Mallikarjun <bhumikamallikarjun98@gmail.com>
2025-01-09 10:34:45 +0100

    removed the mongodb connection

--- api/src/index.js
+++ api/src/index.js
@@ -2,11 +2,9 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import { connectDB } from "../db/database_client.js";
 
-connectDB()
 const app = express();
-const PORT = process.env.PORT || 5000; // default port 5000
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -14,10 +12,8 @@ app.use(bodyParser.json());
 const apiRouter = express.Router();
 
 app.get("/api", async (req, res) => {
-  
   res.json("Welcome to upskill");
 });
-
 
 app.use("/api", apiRouter);
 
