33af0a3de7d4c8bc2efca7af468ff4a3e02ef8a8
Halyna Bondar <galya.marusyak@gmail.com>
2025-01-13 15:11:03 +0100

    Create WelcomePage component

---
+++ app/app/welcome/page.jsx
@@ -0,0 +1,12 @@
+import Button from "@/components/Button";
+import styles from '../../styles/global.css'
+
+function WelcomePage() {
+    return (
+        <>
+            <Button styles={`mt-10`} />
+        </>
+    );
+}
+
+export default WelcomePage;
