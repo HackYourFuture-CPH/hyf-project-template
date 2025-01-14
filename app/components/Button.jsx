d9c0d00b4d43b956475831df781c8bf5239365bd
Halyna Bondar <galya.marusyak@gmail.com>
2025-01-14 10:40:38 +0100

    Add value to the Button

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Button = ({styles}) => {
+const Button = ({styles, value}) => {
     return (
-        <button type="button" className={`py-2 px-6 w-fit text-[18px] font-bold bg-[#636AE8] text-white rounded-[10px] outline-none hover:bg-blue-700 duration-700 ${styles}`}>
-            Log ind
+        <button type="button" className={`py-2 px-6 w-fit text-[18px] font-bold bg-blue-500 text-white rounded-[10px] outline-none hover:bg-blue-700 duration-500 ${styles}`}>
+            {value}
         </button>
     );
 }
