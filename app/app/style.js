9731909198d7b12afed4c7de18ff571ba4d161b0
Halyna Bondar <galya.marusyak@gmail.com>
2025-01-14 10:40:52 +0100

    Change styles

--- app/app/style.js
+++ app/app/style.js
@@ -5,15 +5,15 @@ export const style = {
     flexCenter: "flex flex-col justify-center items-center",
     flexStart: "flex flex-col justify-center items-start",
 
-    paddingX: "sm:px-16 px-6",
-    paddingY: "sm:py-16 py-6",
-    padding: "sm:px-16 px-6 sm:py-12 py-4",
+    paddingX: "sm:px-4 px-2",
+    paddingY: "sm:py-4 py-2",
+    padding: "sm:px-4 px-2 sm:py-4 py-2",
 
-    marginX: "sm:mx-16 mx-6",
-    marginY: "sm:my-16 my-6",
+    marginX: "sm:mx-4 mx-2",
+    marginY: "sm:my-14 my-6",
 };
 
 export const layout = {
-    section: `flex flex-col ${style.paddingY}`,
+    section: `flex ${style.paddingY}`,
     sectionImg: `flex-1 flex ${style.flexCenter} md:ml-10 ml-0 md:mt-0 mt-10 relative`,
 };
