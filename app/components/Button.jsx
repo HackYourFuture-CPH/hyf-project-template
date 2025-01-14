3cc3d2a0d2ffea9678e68932eb7fc3141cd115f8
Halyna Bondar <galya.marusyak@gmail.com>
2025-01-14 07:48:33 +0100

    Change styles

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = ({styles}) => {
     return (
-        <button type="button" className={`py-4 px-6 text-[18px] rounded-[10px] outline-none ${styles}`}>
+        <button type="button" className={`py-2 px-6 w-fit text-[18px] font-bold bg-[#636AE8] text-white rounded-[10px] outline-none hover:bg-blue-700 duration-700 ${styles}`}>
             Log ind
         </button>
     );
