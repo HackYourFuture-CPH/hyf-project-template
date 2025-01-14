880e18da7d0532306e92bb878e83b9f5829fe439
Halyna Bondar <galya.marusyak@gmail.com>
2025-01-14 10:43:23 +0100

    Create Button component

---
+++ app/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = ({styles, value}) => {
+    return (
+        <button type="button" className={`py-2 px-6 w-fit text-[18px] font-bold bg-blue-500 text-white rounded-[10px] outline-none hover:bg-blue-700 duration-500 ${styles}`}>
+            {value}
+        </button>
+    );
+}
+
+export default Button;
