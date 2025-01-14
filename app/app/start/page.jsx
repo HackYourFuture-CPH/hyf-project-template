395cf025e0241da6f3270a369b893791e5528edb
Halyna Bondar <galya.marusyak@gmail.com>
2025-01-14 14:47:35 +0100

    Replace icons with Fontawesome icons

--- app/app/start/page.jsx
+++ app/app/start/page.jsx
@@ -1,28 +1,32 @@
 import Button from "@/components/Button";
 import {style, layout} from "@/app/style";
 import Explore from "../../components/Explore.jsx";
+import {faCircleDollarToSlot} from "@fortawesome/free-solid-svg-icons";
+import {faVolumeUp} from "@fortawesome/free-solid-svg-icons";
+import {faLanguage} from "@fortawesome/free-solid-svg-icons";
+import {faComments} from "@fortawesome/free-regular-svg-icons";
 
 function WelcomePage() {
 
     const exploreFeatures = [
         {
             id: 1,
-            icon: "/text-to-speech.jpg",
+            icon: faVolumeUp,
             title: "Text to Speech"
         },
         {
             id: 2,
-            icon: "/translate.png",
+            icon: faLanguage,
             title: "Translation"
         },
         {
             id: 3,
-            icon: "/free2.jpg",
+            icon: faCircleDollarToSlot,
             title: "Free of Charge"
         },
         {
             id: 4,
-            icon: "/blog.webp",
+            icon: faComments,
             title: "Chat Blog"
         }
     ];
@@ -31,7 +35,7 @@ function WelcomePage() {
         <section>
             <div className={`${layout.section} gap-10`}>
                 <div className={`${style.flexCenter} max-w-[600px]`}>
-                    <h1 className={`${style.heading}`}>Velkommen til DKTestPrep</h1>
+                    <h1 className={`${style.heading}`}>Velkommen <br/> til DKTestPrep</h1>
                     <p className={`${style.paragraph} my-6`}>Bliv klar til den danske indfødsretsprøve med selvtillid!
                         Forbereder du dig på den danske indfødsretsprøve? DKTestPrep er her for at hjælpe dig med at få
                         succes. Med vores interaktive øvequizzer og engagerende fællesskabsblog vil du få den viden og
