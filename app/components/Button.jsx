2e9d99bca7d54e01716c0e2b499bd531c361f7b4
Halyna Bondar <galya.marusyak@gmail.com>
2025-01-16 17:53:21 +0100

    Add type property for button

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -2,14 +2,14 @@
 
 import React from 'react';
 
-const Button = ({styles, value, onClick}) => {
+const Button = ({styles, value, onClick, type='button'}) => {
     
     const onClickHandler = () => {
         onClick();
     }
 
     return (
-        <button onClick={onClickHandler} type="button" className={`py-2 px-6 w-fit text-[18px] font-bold bg-blue-500 text-white rounded-[10px] outline-none hover:bg-blue-700 duration-500 ${styles}`}>
+        <button onClick={onClickHandler} type={type} className={`py-2 px-6 w-fit text-[18px] font-bold bg-blue-500 text-white rounded-[10px] outline-none hover:bg-blue-700 duration-500 ${styles}`}>
             {value}
         </button>
     );
