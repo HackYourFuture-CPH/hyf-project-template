50c6e0c369672f18177a7507001264efc3e8e73f
Halyna Bondar <galya.marusyak@gmail.com>
2025-01-16 21:20:18 +0100

    Refactor Button component to simplify onClick handling

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -3,15 +3,12 @@
 import React from 'react';
 
 const Button = ({styles, value, onClick, type = 'button'}) => {
-
-    const onClickHandler = () => {
-        if (onClick)
-            onClick();
-    }
-
     return (
-        <button onClick={onClickHandler} type={type}
-                className={`py-2 px-6 w-fit text-[18px] font-bold bg-blue-500 text-white rounded-[10px] outline-none hover:bg-blue-700 duration-500 ${styles}`}>
+        <button
+            onClick={onClick}
+            type={type}
+            className={`py-2 px-6 w-fit text-[18px] font-bold bg-blue-500 text-white rounded-[10px] outline-none hover:bg-blue-700 duration-500 ${styles}`}
+        >
             {value}
         </button>
     );
