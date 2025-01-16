db1b5efe11d8349b3526ef2597abe36aa5590dc4
Halyna Bondar <galya.marusyak@gmail.com>
2025-01-16 17:55:27 +0100

    Make onClick callback property optional for Button

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -2,14 +2,16 @@
 
 import React from 'react';
 
-const Button = ({styles, value, onClick, type='button'}) => {
-    
+const Button = ({styles, value, onClick, type = 'button'}) => {
+
     const onClickHandler = () => {
-        onClick();
+        if (onClick)
+            onClick();
     }
 
     return (
-        <button onClick={onClickHandler} type={type} className={`py-2 px-6 w-fit text-[18px] font-bold bg-blue-500 text-white rounded-[10px] outline-none hover:bg-blue-700 duration-500 ${styles}`}>
+        <button onClick={onClickHandler} type={type}
+                className={`py-2 px-6 w-fit text-[18px] font-bold bg-blue-500 text-white rounded-[10px] outline-none hover:bg-blue-700 duration-500 ${styles}`}>
             {value}
         </button>
     );
