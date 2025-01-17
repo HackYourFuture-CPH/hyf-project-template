edbb58d3609788209351c013f55dac12e49155a6
SpringrollBrothers <luliu.vln@gmail.com>
2025-01-17 20:45:03 +0100

    fix footer position accordingly

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="flex flex-col min-h-screen">
       <div className="flex-grow">
       </div>
-      <footer className="bg-gray-100 text-black py-4 border-t">
+      <footer className="absolute bottom-0 w-full h-10 bg-gray-100 text-black py-4 border-t">
         <div className="container mx-auto flex justify-between items-center">
           <p className="text-sm">
             &copy; 2025 CodeCrafters Inc. | Privatlivspolitik | Copyright
