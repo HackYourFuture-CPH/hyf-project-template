e2fd5cd444e27c07fd8cc19bd0209e0dc6ca5fe0
SpringrollBrothers <luliu.vln@gmail.com>
2025-01-17 20:30:52 +0100

    footer updated accordingly,so it will stay at the bottom when there is content

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -2,13 +2,17 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-100 text-black py-4 border-t">
-      <div className="container mx-auto flex justify-between items-center">
-        <p className="text-sm">
-          &copy; 2025 CodeCrafters Inc. | Privatlivspolitik | Copyright
-        </p>
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow">
       </div>
-    </footer>
+      <footer className="bg-gray-100 text-black py-4 border-t">
+        <div className="container mx-auto flex justify-between items-center">
+          <p className="text-sm">
+            &copy; 2025 CodeCrafters Inc. | Privatlivspolitik | Copyright
+          </p>
+        </div>
+      </footer>
+    </div>
   );
 };
 
