818ca14e809535d586d3e7b5e278a548933aebb7
SpringrollBrothers <luliu.vln@gmail.com>
2025-01-20 10:11:46 +0100

    updated to dynamic year

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <footer className="absolute bottom-0 w-full h-10 bg-gray-100 text-black py-4 border-t">
         <div className="container mx-auto flex justify-between items-center">
           <p className="text-sm">
-            &copy; 2025 CodeCrafters Inc. | Privatlivspolitik | Copyright
+          &copy; {new Date().getUTCFullYear()} CodeCrafters Inc. | Privatlivspolitik | Copyright
           </p>
         </div>
       </footer>
