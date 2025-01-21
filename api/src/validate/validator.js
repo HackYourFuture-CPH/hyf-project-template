d7929864f0cb008fbe5ac69e3cca782182b65e9b
Bhumika Mallikarjun <bhumikamallikarjun98@gmail.com>
2025-01-21 22:35:21 +0100

    changed the token expiry to one day

--- api/src/validate/validator.js
+++ api/src/validate/validator.js
@@ -17,7 +17,7 @@ export const signUpSchema = Joi.object({
 
 export const loginSchema = Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().min(8).required(),
+  password: Joi.string().required(),
 });
 
 export const validateRequest = (schema) => {
