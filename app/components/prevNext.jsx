09423efe0f0b69ab260e3e504e4dfcd0566f00d3
Dennis Agyekum <kinggbadi@gmail.com>
2025-01-21 12:16:57 +0100

    this component allow users to skip through questions on the exams page

--- app/components/prevNext.jsx
+++ app/components/prevNext.jsx
@@ -3,17 +3,17 @@ import PropTypes from 'prop-types';
 
 function PreviousNext({ showPrevious, showNext, onPrevious, onNext }) {
   return (
-    <div className="mt-12 flex max-w-5xl justify-between">
+    <div className="mx-auto mt-12 flex max-w-[738px] justify-between">
       {showPrevious && (
         <Button
-          styles="bg-blue-500 hover:bg-blue-700"
+          styles=" bg-blue-500 hover:bg-blue-700"
           value="Previous"
           onClick={onPrevious}
         />
       )}
       {showNext && (
         <Button
-          styles="bg-blue-500 hover:bg-blue-700"
+          styles="pl-11 pr-11 bg-blue-500 hover:bg-blue-700"
           value="Next"
           onClick={onNext}
         />
