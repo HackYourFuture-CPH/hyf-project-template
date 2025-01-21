a43d0456173962a817d4845b9b2040cb85a08860
Oyelaja Hussein <premiumitng@gmail.com>
2025-01-21 14:36:44 +0100

    Created the testimonial component

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,10 +1,12 @@
 import HeroSection from "./_components/HomePageComponents/Hero";
 import CategorySection from "./_components/HomePageComponents/CategorySection";
+import Testimonial from "./_components/HomePageComponents/Testimonial";
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <CategorySection />
+      <Testimonial />
     </div>
   );
 }
