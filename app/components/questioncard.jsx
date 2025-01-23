6b7b02a7a038cabd56513f7a97eb343fa68e91b9
Dennis Agyekum <kinggbadi@gmail.com>
2025-01-23 14:58:33 +0100

    next button always on the right, the user can also see the selected answer when previous button is clicked

--- app/components/questioncard.jsx
+++ app/components/questioncard.jsx
@@ -1,16 +1,20 @@
 import PropTypes from 'prop-types';
 
-function QuestionCard({ question, answers, onAnswer }) {
+function QuestionCard({ question, answers, selectedAnswer, onAnswer }) {
   return (
     <div className="mx-auto w-full max-w-4xl rounded-xl bg-white p-10">
       <h2 className="mb-10 text-center text-2xl font-bold text-gray-800">
         {question}
       </h2>
       <div className="flex flex-col items-center gap-3">
-        <div className="flex flex-col items-center gap-3">
+        <div className="flex w-full flex-col items-center gap-3">
           {Object.keys(answers).map((key) => (
             <button
-              className="w-full max-w-3xl cursor-pointer rounded-lg border border-gray-100 bg-gray-100 p-1 text-lg shadow-md transition-colors duration-300 focus:outline-none"
+              className={`w-full max-w-3xl cursor-pointer rounded-lg border p-2 text-lg shadow-md transition-colors duration-300 focus:outline-none ${
+                selectedAnswer === key
+                  ? 'border-gray-100  bg-gray-400 text-black'
+                  : 'border-gray-100 bg-gray-100 text-gray-800'
+              }`}
               onClick={() => onAnswer(key)}
               key={key}
             >
@@ -26,6 +30,7 @@ function QuestionCard({ question, answers, onAnswer }) {
 QuestionCard.propTypes = {
   question: PropTypes.string.isRequired,
   answers: PropTypes.object.isRequired,
+  selectedAnswer: PropTypes.string,
   onAnswer: PropTypes.func.isRequired,
 };
 
