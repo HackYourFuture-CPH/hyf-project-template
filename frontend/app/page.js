b83263d1cb86cc0ed498384ebdbfd986fec41781
RajeshBhatt <rjshbhatt9@gmail.com>
2025-01-23 13:17:07 -0800

    created the instructor component fot the home page

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,10 +1,12 @@
 import HeroSection from "./_components/HomePageComponents/Hero";
 import CategorySection from "./_components/HomePageComponents/CategorySection";
+import InstructorSection from "./_components/HomePageComponents/InstructorSection";
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <CategorySection />
+      <InstructorSection />
     </div>
   );
 }
