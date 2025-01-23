6adda0cec328772133248b4de9b07bb04843220c
Dennis Agyekum <kinggbadi@gmail.com>
2025-01-23 15:04:32 +0100

    suggestions added

--- app/components/prevNext.jsx
+++ app/components/prevNext.jsx
@@ -6,11 +6,12 @@ function PreviousNext({ showPrevious, showNext, onPrevious, onNext }) {
     <div className="mx-auto mt-12 flex max-w-[738px] justify-between">
       {showPrevious && (
         <Button
-          styles=" bg-blue-500 hover:bg-blue-700"
+          styles="bg-blue-500 hover:bg-blue-700"
           value="Previous"
           onClick={onPrevious}
         />
       )}
+      <div className="flex-1"></div>
       {showNext && (
         <Button
           styles="pl-11 pr-11 bg-blue-500 hover:bg-blue-700"
@@ -21,6 +22,7 @@ function PreviousNext({ showPrevious, showNext, onPrevious, onNext }) {
     </div>
   );
 }
+
 PreviousNext.propTypes = {
   showPrevious: PropTypes.bool.isRequired,
   showNext: PropTypes.bool.isRequired,
