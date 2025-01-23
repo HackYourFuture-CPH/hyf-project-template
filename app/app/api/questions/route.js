4108ad66b55d444161b9990f555f22273c823389
Dennis Agyekum <kinggbadi@gmail.com>
2025-01-23 14:47:15 +0100

    the backend now fetches only the questions and answers, exluding the correct answers as suggesred another function added to compare user answers to the correct answer

--- app/app/api/questions/route.js
+++ app/app/api/questions/route.js
@@ -3,11 +3,41 @@ import knex from '@/app/api/knex';
 
 export async function GET() {
   try {
-    const result = await knex('questions').orderByRaw('RANDOM()').limit(40);
+    const questions = await knex('questions')
+      .select('id', 'question', 'answers')
+      .orderByRaw('RANDOM()')
+      .limit(40);
 
-    return NextResponse.json(result);
+    return NextResponse.json(questions);
   } catch (error) {
     console.error('Error fetching questions:', error.message);
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 },
+    );
+  }
+}
+
+export async function POST(req) {
+  try {
+    const userAnswers = await req.json();
+
+    const questionIds = Object.keys(userAnswers);
+
+    const correctAnswers = await knex('questions')
+      .whereIn('id', questionIds)
+      .select('id', 'correctanswers');
+
+    const results = correctAnswers.map((question) => ({
+      questionId: question.id,
+      isCorrect: userAnswers[question.id] === question.correctanswers,
+    }));
+
+    const score = results.filter((result) => result.isCorrect).length;
+
+    return NextResponse.json({ results, score });
+  } catch (error) {
+    console.error('Error validating answers:', error.message);
     return NextResponse.json(
       { error: 'Internal Server Error' },
       { status: 500 },
