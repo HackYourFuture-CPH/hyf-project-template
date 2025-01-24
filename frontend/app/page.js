0e5ba9dda33ec9e5133d5ace452f7262efb0432c
Hanna Vorontsova <159482282+antuanettaV@users.noreply.github.com>
2025-01-24 21:53:00 +0200

    updated faq file

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,10 +1,12 @@
 import HeroSection from "./_components/HomePageComponents/Hero";
 import CategorySection from "./_components/HomePageComponents/CategorySection";
+import FAQ from "./_components/HomePageComponents/FAQ";
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <CategorySection />
+      <FAQ />
     </div>
   );
 }
