2029f3cdd03c8d64fc7fe403ac03ed4450de0b8d
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-01-26 16:07:19 +0100

    Update footer

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -1,16 +1,14 @@
 const Footer = () => {
   return (
-    <div className="flex  min-h-screen flex-col">
-      <div className="grow"></div>
-      <footer className="absolute bottom-0 h-10 w-full border-t bg-gray-100 py-4 text-black">
-        <div className="container mx-auto flex items-center justify-between">
-          <p className="text-sm">
-            &copy; {new Date().getUTCFullYear()} CodeCrafters Inc. |
-            Privatlivspolitik | Copyright
-          </p>
-        </div>
-      </footer>
-    </div>
+    <footer className="w-full bg-blue-100 py-7 text-gray-700">
+      <div className="container mx-auto flex items-center justify-center gap-10">
+        <p className="text-sm font-semibold">
+          &copy; {new Date().getUTCFullYear()} CodeCrafters Inc.
+        </p>
+        <p className="text-sm font-semibold">Privatlivspolitik</p>
+        <p className="text-sm font-semibold">Copyright</p>
+      </div>
+    </footer>
   );
 };
 
