7c40adb73d02d1194f5f94e4158b34e21c1a429f
melo4yxa77796 <g_julija_777@mail.ru>
2025-01-27 11:17:37 +0100

    put back rate us server action

--- app-next/action.js
+++ app-next/action.js
@@ -152,3 +152,21 @@ export async function updateProfile(userId, dob, avatarUrl) {
     throw new Error("Failed to update profile.");
   }
 }
+export async function saveReview(formData) {
+  const rating = formData.get("rating");
+  const title = formData.get("title");
+  const review = formData.get("review");
+  console.log("Received data in saveReview:", { rating, title, review });
+  try {
+    await connection("reviews").insert({
+      rating: parseInt(rating, 10),
+      title,
+      review,
+    });
+    console.log("Review successfully saved.");
+    return { success: true };
+  } catch (error) {
+    console.error("Error saving review to database:", error.message);
+    throw new Error("Failed to save review.");
+  }
+}
