cd652d1cf48cf9a796777bb1a237a42d4737a7d7
melo4yxa77796 <g_julija_777@mail.ru>
2025-01-27 12:49:23 +0100

    save comment from footer form contact

--- app-next/action.js
+++ app-next/action.js
@@ -170,3 +170,26 @@ export async function saveReview(formData) {
     throw new Error("Failed to save review.");
   }
 }
+
+export async function saveContactMessage(formData) {
+  const { name, email, message } = formData;
+
+  console.log("Received contact form data:", { name, email, message });
+
+  try {
+    const currentTime = new Date().toISOString();
+
+    const result = await connection("contact_messages").insert({
+      name,
+      email,
+      message,
+      created_at: currentTime,
+    });
+
+    console.log("Contact message successfully saved:", result);
+    return { success: true };
+  } catch (error) {
+    console.error("Error saving contact message:", error);
+    throw new Error("Failed to save contact message.");
+  }
+}
