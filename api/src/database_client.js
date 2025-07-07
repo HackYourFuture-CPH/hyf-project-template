f4a7c5d3e21d6c25308ccc3e9b438fa73221a483
Adam Blanchard <adamgoodie@gmail.com>
2025-07-07 10:51:16 +0200

    testing local sqlite support

--- api/src/database_client.js
+++ api/src/database_client.js
@@ -8,9 +8,11 @@ const connection = knex({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE_NAME,
+    filename: process.env.DB_SQLITE_FILENAME, // For SQLite
     ssl:
       process.env.DB_USE_SSL === "true" ? { rejectUnauthorized: false } : false,
   },
+  useNullAsDefault: process.env.DB_USE_NULL_AS_DEFAULT === "true", // For SQLite
 });
 
 export default connection;
