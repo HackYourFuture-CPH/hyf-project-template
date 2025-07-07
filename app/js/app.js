1ed68ebe4331878dfa35a9e722302c78b1af0a01
Adam Blanchard <adamgoodie@gmail.com>
2025-07-07 13:15:46 +0200

    test api url

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,6 @@
 // App code goes here
+
+// If you have defined an API_URL in your environment variables, you can grab it like this:
+const API_URL = import.meta.env.API_URL;
+
+console.log('API URL:', API_URL);
