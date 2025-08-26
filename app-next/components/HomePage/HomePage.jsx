ad751b47fb9bd1c938beab722803ab6ca715b3ea
mateus_britto <mateusbritto00@gmail.com>
2025-08-26 14:32:20 +0200

    Create homepage with hero section, image transition, and search box

--- app-next/components/HomePage/HomePage.jsx
+++ app-next/components/HomePage/HomePage.jsx
@@ -1,17 +1,11 @@
-import HYFLogo from "@/assets/hyf.svg";
-import Image from "next/image";
 import "./HomePage.css";
+import HeroSection from "../HeroSection/HeroSection";
 
 // Feel free to replace the content of this component with your own
 function HomePage() {
   return (
     <>
-      <a href="https://www.hackyourfuture.dk/" target="_blank" className="link">
-        <Image src={HYFLogo.src} width={HYFLogo.width} height={HYFLogo.height} className="logo" />
-      </a>
-      <a href="/nested" className="link">
-        <span className="message">Go to the nested page</span>
-      </a>
+      <HeroSection />
     </>
   );
 }
