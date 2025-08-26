b2c949e489424a32bf3b4fdcb4b9063a50211e5e
nigerabed <nigerabed@gmail.com>
2025-08-26 18:01:24 +0200

    Header and footer style in progress

--- app-next/app/page.js
+++ app-next/app/page.js
@@ -2,6 +2,7 @@ import HomePage from "@/components/HomePage/HomePage";
 
 export default function Home() {
   return (
-    <HomePage />
+    <></>
+    // <HomePage />
   );
 }
--- app-next/components/HomePage/HomePage.jsx
+++ app-next/components/HomePage/HomePage.jsx
@@ -2,10 +2,15 @@ import HYFLogo from "@/assets/hyf.svg";
 import Image from "next/image";
 import "./HomePage.css";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+
 // Feel free to replace the content of this component with your own
 function HomePage() {
   return (
     <>
+    <FontAwesomeIcon icon={faCoffee}  style={{ color: "blue", width: "32px" }} />
+
       <a href="https://www.hackyourfuture.dk/" target="_blank" className="link">
         <Image src={HYFLogo.src} width={HYFLogo.width} height={HYFLogo.height} className="logo" />
       </a>
