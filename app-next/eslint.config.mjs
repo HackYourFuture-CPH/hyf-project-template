73dfe9ad76c006bfffcf5294c4a7ad9a831dd1cb
Vadim Tofan <vad.tofan@gmail.com>
2025-08-26 21:46:53 +0200

    Fix Eslint issues

--- app-next/eslint.config.mjs
+++ app-next/eslint.config.mjs
@@ -12,13 +12,11 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals"),
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"],
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
+    },
   },
 ];
 
