400901e797105e70f8373d388cf1a107e1d1601d
nigerabed <nigerabed@gmail.com>
2025-08-29 09:24:53 +0200

    added travel card component,travel card css file,added mock json data

--- app-next/app/page.js
+++ app-next/app/page.js
@@ -1,9 +1,12 @@
 import HomePage from "@/components/HomePage/HomePage";
+import Card from "@/components/Card/Card";
+import TravelCards from "@/components/TravelCards/TravelCards";
 
 export default function Home() {
   return (
     <>
       <HomePage />
+      <TravelCards />
     </>
   );
 }
