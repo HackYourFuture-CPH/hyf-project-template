3151252a33338c392d045a52512834e4533a7250
Yasi Ayazi <yasi.ayazi@gmail.com>
2025-08-30 12:36:54 +0200

    Refactor Navbar component to use Image for logo and update styles for logo text

--- app-next/components/Navbar/Navbar.js
+++ app-next/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import { User } from 'lucide-react';
 import styles from './Navbar.module.css';
 
@@ -9,23 +10,26 @@ const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <div className={styles.container}>
-        {/* Logo */}
         <div className={styles.logo}>
-          <span className={styles.logoText}>PetPass</span>
+          <Image
+            className={styles['logo__img']}
+            src="/images/logo.png"
+            alt="PetPass logo"
+            width={120}
+            height={120}
+            priority
+          />
+          <span className={styles['logo__text']}>PetPass</span>
         </div>
 
-        {/* Navigation Items */}
         <ul className={styles.navItems}>
           {navItems.map((item, index) => (
             <li key={index} className={styles.navItem}>
-              <a href="#" className={styles.navLink}>
-                {item}
-              </a>
+              <a href="#" className={styles.navLink}>{item}</a>
             </li>
           ))}
         </ul>
 
-        {/* Login Icon */}
         <div className={styles.loginSection}>
           <button className={styles.loginButton}>
             <User size={20} />
