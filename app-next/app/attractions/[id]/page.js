fd1090b340d14d20d500b8922206016ff22b4755
nigerabed <nigerabed@gmail.com>
2025-09-04 10:52:57 +0200

    fixed footer logo and style

---
+++ app-next/app/attractions/[id]/page.js
@@ -0,0 +1,48 @@
+"use client";
+import styles from "./attractionDetails.module.css";
+import { useParams } from "next/navigation";
+import attractionData from "../../../mockData/BlogPostsData.json";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export default function AttractionDetailsPage() {
+  const { id } = useParams();
+const [attraction, setAttraction] = useState(null);
+  
+  useEffect(() => {
+    if (id) {
+        // Api Fetch the attraction details based on the id
+
+        // TODO: Replace with actual data fetching logic
+        const found = attractionData.find((item) => item.id === parseInt(id));
+console.log("Found attraction:", found);
+
+      setAttraction(found);
+    }
+  }, [id]);
+
+  if (!attraction) return <p>Loading...</p>;
+
+return (
+     <div className={styles.pageWrapper}>
+    <div className={styles.travelCard}>
+      <div className={styles.imageWrapper}>
+        <Image
+          src={attraction.image}
+          alt={attraction.destination || "Blog image"}
+          fill
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+      <div className={styles.cardContent}>
+        <h4 className={styles.cardTitle}>{attraction.title}</h4>
+        <div className={styles.meta}>
+          <span>{attraction.country}</span>
+        </div>
+        <p className={styles.description}>{attraction.excerpt}</p>
+      </div>
+
+    </div>
+    </div>
+  );
+}
---
+++ app-next/app/attractions/page.js
@@ -0,0 +1,5 @@
+import styles from "./attractions.module.css";
+
+export default function AttractionsPage() {
+  return <div>Attractions Page</div>;
+}
